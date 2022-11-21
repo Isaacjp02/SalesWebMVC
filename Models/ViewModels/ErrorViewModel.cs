123a5e9898acc3623a92b2c488bcd17eff3491a9
Isaac Lima <isaacjp02@gmail.com>
2022-11-21 10:09:59 -0300

    Add package for project

--- Models/ViewModels/ErrorViewModel.cs
+++ Models/ViewModels/ErrorViewModel.cs
@@ -1,8 +1,8 @@
-namespace SalesWebMVC.Models;
+namespace SalesWebMVC.Models.ViewModels;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
