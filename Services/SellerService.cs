06f0dc5f5e7e91ca5dc6af29f3955850a13c356b
Isaac Lima <isaacjp02@gmail.com>
2022-11-30 13:11:50 -0300

    Tratando erros de integridade referencial do banco de dados

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -44,9 +44,17 @@ namespace SalesWebMVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+
+                throw new IntegrityException("Não pode deletar o vendedor pois eles está associado!");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
