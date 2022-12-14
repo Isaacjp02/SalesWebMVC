089c4d8a2599a21d2b4a1d590fa93c66d45b6bb5
Isaac Lima <isaacjp02@gmail.com>
2022-12-14 15:01:47 -0300

    Verificar GruouBy Classe SalesRecordsServices

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -36,7 +36,7 @@ namespace SalesWebMVC.Models
         {
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department, int departmentId, List<SalesRecord> sales)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department, int departmentId)
         {
             Id = id;
             Name = name;
@@ -45,7 +45,7 @@ namespace SalesWebMVC.Models
             BaseSalary = baseSalary;
             Department = department;
             DepartmentId = departmentId;
-            Sales = sales;
+            
         }
 
         // Metodos
